fc0498b5770dd5f6d2d630c6ef8584992c76963b
aanitapandit123 <aanitapandit@gmail.com>
2019-12-24 15:03:34 +0545

    node_modules to be deleted

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import Landing from './components/layout/Landing';
+// import Navbar from './components/layout/Navbar';
+
+import Routers from './components/routes/Router';
+const App = () => (
+  <Fragment>
+    <Routers />
+  </Fragment>
+
+);
+
+
+
+export default App;
