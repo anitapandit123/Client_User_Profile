b51465470c8183f017b40bb91102118c7aa49308
aanitapandit123 <aanitapandit@gmail.com>
2020-03-12 17:25:55 +0545

    education.js

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import DashboardActions from './DashboardActions';
 
 export const PrivateRoute = ({ component: Component, auth: { isAuthenticated, loading }, ...rest }) => (
     <Route {...rest} render={props => !isAuthenticated && !loading ? (<Redirect to="/login" />) : (<Component {...props} />)}
