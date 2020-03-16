94d67f503deff75a2691a26b0d0840dd2d0a2ad0
aanitapandit123 <aanitapandit@gmail.com>
2020-03-16 13:29:17 +0545

    PostForm.js

--- src/components/posts/post.js
+++ src/components/posts/post.js
@@ -7,7 +7,7 @@ import { getPosts } from '../../actions/post';
 
 
 
-const Posts => ({ getPosts, post: { posts, loading } }) => {
+const Posts = ({ getPosts, post: { posts, loading } }) => {
     useEffect(() => {
         getPosts();
     }, [getPosts]);
