cd82d56cde1b54376cea696b06625338fe5a94be
aanitapandit123 <aanitapandit@gmail.com>
2020-04-03 17:56:26 +0545

    Add Education Form Finished

--- src/components/profile-forms/AddEducation.js
+++ src/components/profile-forms/AddEducation.js
@@ -34,6 +34,84 @@ const AddEducation = ({ addEducation, history }) => {
     return (
         <Fragment>
             <h1 className='large text-primary'>Add Your Education</h1>
-        </Fragment>
+            <p className='lead'>
+                <i className='fas fa-code-branch' /> Add any school or bootcamp that you
+        have attended
+      </p>
+            <small>* = required field</small>
+            <form
+                className="form"
+                onSubmit={e => {
+                    e.preventDefault();
+                    addEducation(formData, history)
+                }}
+            >
+                <div className="form-group">
+                    <input
+                        type="text"
+                        placeholder="* School or Bootcamp"
+                        name="degree"
+                        value={degree}
+                        onChange={e => onChange(e)}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type='text'
+                        placeholder="* Degree or Certificate"
+                        name="degree"
+                        value={degree}
+                        onChange={e => onChange(e)}
+                        required
+                    />
+                </div>
+                <div className='form-group'>
+                    <h4>Form Date</h4>
+                    <input
+                        type="date"
+                        name="form"
+                        value={from}
+                        onChange={e => onChange(e)}
+                    />
+                </div>
+                <div className="form-group">
+                    <p>
+                        <input
+                            type="checkbox"
+                            name="current"
+                            checked={current}
+                            value={current}
+                            onChange={() => {
+                                setFormData({ ...formData, current: !current });
+                                toggleDisabled(!toDateDisabled);
+                            }}
+                        /> {''}
+                        Current school
+                    </p>
+                </div>
+                <div className="form-group">
+                    <h4>To Date</h4>
+                    <input
+                        type="date"
+                        name="to"
+                        value={to}
+                        onChange={e => onChange(e)}
+                        disabled={toDateDisabled ? 'disabled' : ''}
+                    />
+                    <input type="submit" className="btn btn-primary my-1" />
+                    <Link className="btn btn-light my-1" to="/dashboard">Go Back</Link>
+                </div>
+            </form>
+        </Fragment >
     )
-}
+};
+
+AddEducation.propTypes = {
+    addEducation: PropTypes.func.isRequired
+};
+
+export default connect(
+    null,
+    { addEducation }
+)(withRouter(AddEducation));
